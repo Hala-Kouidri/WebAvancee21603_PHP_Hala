1b8e06d8fc09e77b7e16b8cafabd2ee29c12db9a
Hala-Kouidri <e1353390@cmaisonneuve.qc.ca>
2022-06-22 19:18:57 -0400

    seance 12

---
+++ seance12_Hala/controller/ControllerHome.php
@@ -0,0 +1,20 @@
+<?php
+
+class ControllerHome{
+
+    public function index(){
+
+        $view = new view('home-index');
+        
+    }
+
+    public function error(){
+
+        $view = new view('error');
+        
+    }
+}
+
+
+
+
